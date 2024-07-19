39449fc70e0f80b63dbec8afc679732802012aa8
drd00 <dohertd3@tcd.ie>
2024-07-19 13:03:10 +0100

    Add confidential data, offensive data, subpopulations, identifiable individuals and sensitive data SHACL constraints

--- dataset-documentation/decision-trees/install-with-venv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt install graphviz graphviz-dev && \
-python3 -m venv ./venv/ && source ./venv/bin/activate && pip install -r requirements.txt
--- dataset-documentation/decision-trees/main.py
+++
@@ -1,5 +0,0 @@
-import pygraphviz as pgv
-
-if __name__ == "__main__":
-    G = pgv.AGraph(directed=True)
-
