90c5cabf4cc5295eb912e359fd6865e9f6bcc119
drd00 <dohertd3@tcd.ie>
2024-07-31 14:38:50 +0100

    Add files for Jena/Topbraid SHACL validation

---
+++ dataset-documentation/jena-eval/jenaeval/build-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mvn clean package
+java -jar ./target/jenaeval-1.0-SNAPSHOT.jar
---
+++ dataset-documentation/jena-eval/jenaeval/src/main/java/com/example/App.java
@@ -0,0 +1,29 @@
+package com.example;
+import java.io.StringWriter;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.RDFDataMgr;
+import org.topbraid.shacl.validation.ValidationUtil;
+import org.topbraid.shacl.vocabulary.SH;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Model dataGraph = RDFDataMgr.loadModel("rdf/use-case-chexpert.ttl");
+        Model shapeGraph = RDFDataMgr.loadModel("rdf/datasheets-shapes-copy.ttl");
+        Model classDefinitions = RDFDataMgr.loadModel("rdf/datasheets-dcat-dqv.ttl");
+
+        dataGraph.add(classDefinitions);
+
+        Resource report = ValidationUtil.validateModel(dataGraph, shapeGraph, true);
+
+        System.out.println(report.getModel().write(new StringWriter(), "TURTLE"));
+
+        boolean conforms = report.getProperty(SH.conforms).getBoolean();
+        System.out.println("Conforms: " + conforms);
+    }
+}
