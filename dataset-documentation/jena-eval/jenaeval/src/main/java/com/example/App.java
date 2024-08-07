1b4ac78692b6c50545d51a809a8f51ac8068786d
drd00 <dohertd3@tcd.ie>
2024-08-07 17:51:27 +0100

    Cleanup and add moview use-case file

--- dataset-documentation/jena-eval/jenaeval/src/main/java/com/example/App.java
+++ dataset-documentation/jena-eval/jenaeval/src/main/java/com/example/App.java
@@ -7,23 +7,24 @@ import org.apache.jena.riot.RDFDataMgr;
 import org.topbraid.shacl.validation.ValidationUtil;
 import org.topbraid.shacl.vocabulary.SH;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Checking compliance of datagraph with SHACL shape..." );
-        
-        Model dataGraph = RDFDataMgr.loadModel("rdf/use-case-chexpert.ttl");
-        Model shapeGraph = RDFDataMgr.loadModel("rdf/datasheets-shapes.ttl");
-        Model classDefinitions = RDFDataMgr.loadModel("rdf/datasheets-dcat-dqv.ttl");
+public class App {
+    private static void checkCompliance(String datagraphPath, String shapeGraphPath, String classDefinitionsPath) {
+        Model dataGraph = RDFDataMgr.loadModel(datagraphPath);
+        Model shapeGraph = RDFDataMgr.loadModel(shapeGraphPath);
+        Model classDefinitions = RDFDataMgr.loadModel(classDefinitionsPath);
 
         dataGraph.add(classDefinitions);
-
         Resource report = ValidationUtil.validateModel(dataGraph, shapeGraph, true);
-
         System.out.println(report.getModel().write(new StringWriter(), "TURTLE"));
-
         boolean conforms = report.getProperty(SH.conforms).getBoolean();
         System.out.println("Conforms: " + conforms);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Checking compliance of datagraph with SHACL shape for the CheXpert dataset...");
+        checkCompliance("rdf/use-case-chexpert.ttl", "rdf/datasheets-shapes.ttl", "rdf/datasheets-dcat-dqv.ttl");
+
+        System.out.println("Checking compliance of datagraph with SHACL shape for the Movie review polarity dataset...");
+        checkCompliance("rdf/use-case-moview-review.ttl", "rdf/datasheets-shapes.ttl", "rdf/datasheets-dcat-dqv.ttl");
     }
 }
